cb5130e5cee611ad11dd5f0d9db095b52d42c8ef
Brad D Messner <bmessner@setonhill.edu>
2020-03-08 22:47:12 -0400

    Initial Commit

---
+++ Messner In Class NavTests/Messner_In_Class_NavTests.swift
@@ -0,0 +1,34 @@
+//
+//  Messner_In_Class_NavTests.swift
+//  Messner In Class NavTests
+//
+//  Created by Brad D. Messner on 3/8/20.
+//  Copyright © 2020 Brad D. Messner. All rights reserved.
+//
+
+import XCTest
+@testable import Messner_In_Class_Nav
+
+class Messner_In_Class_NavTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
